bdacaacc4d35596d5a2dc4c7dde063810efe011e
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-13 11:45:29 -0300

    1 file modified, 1 added

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -13,5 +13,21 @@ const openai = new OpenAIApi(configuration);
 router.route('/').get((req, res) => {
   res.send('Hello from DALL-E API');
 });
-
+router.route('/').post(async (req, res) => {
+  try {
+    const prompt =
+      'panda mad scientist mixing sparkling chemicals, digital art';
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 export default router;
